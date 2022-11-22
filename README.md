2687391d0c42a70dbc948ad04f71c83ab6e0c073
Aakarsh Shekhar <aakarsh.shekhar@mail.mcgill.ca>
2022-11-22 16:12:42 -0500

    add finished API with documentation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # boosted_technology_consent_api
+
+Install dependencies:
+`pip3 install -r requirements.txt`
+
+To run:
+`python3 init.py`
+
+App will be running on localhost.
+Example queries:
+
+```
+curl -X GET \
+  -H "Content-type: application/json" \
+  -H "Accept: application/json" \
+  "http://127.0.0.1:5000/consent/target"
+
+
+curl -X POST \
+  -H "Content-type: application/json" \
+  -H "Accept: application/json" \
+  -d '{"name":"pharmacy.allow_marketing_emails", "consent_url":"http://example.com/marketing_terms"}' \
+  "http://127.0.0.1:5000/consent/target"
+
+
+curl -X PATCH \
+  -H "Content-type: application/json" \
+  -H "Accept: application/json" \
+  -d '{"consent_url":"http://example.com/marketing_terms_updated2"}' \
+  "http://127.0.0.1:5000/consent/target/<targetId>"
+```
--- app/main.py
+++ app/main.py
@@ -12,28 +12,22 @@ ma = Marshmallow(app)
 api = Api(app)
 
 class Consent(db.Model):
-    id = db.Column(db.Text(length=36), default=lambda: str(uuid.uuid4()), primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Text(length=36), default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(100), nullable=False)
     consent_url = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.Date, default=datetime.datetime.utcnow)
     version = db.Column(db.Integer, default=0, nullable=False)
-    # Every time a modification is made to a target, the entire row is duplicated,
-    #  the change is applied and this integer is incremented by 1. Starting at 0
 
     def __repr__(self):
         return '<Consent %s>' % self.name
 
-# only run once on init
 with app.app_context():
     db.create_all()
-
-    # db.session.add(User('admin', 'admin@example.com'))
-    # db.session.add(User('guest', 'guest@example.com'))
-    # db.session.commit()
-
-    consent = Consent.query.all()
-    print(consent)
-    print("***")
+    # -- sanity check --
+    # consent = Consent.query.all()
+    # print(consent)
+    # print("***")
 
 class ConsentSchema(ma.Schema):
     class Meta:
@@ -47,34 +41,36 @@ class ConsentTargetsResource(Resource):
         all_targets = Consent.query.all()
         return consents_schema.dump(all_targets)
 
-    # def post(self):
-    #     new_post = Post(
-    #         title=request.json['title'],
-    #         content=request.json['content']
-    #     )
-    #     db.session.add(new_post)
-    #     db.session.commit()
-    #     return post_schema.dump(new_post)
+    def post(self):
+        new_consent = Consent(
+            name=request.json['name'],
+            consent_url=request.json['consent_url']
+        )
+        db.session.add(new_consent)
+        db.session.commit()
+        return consent_schema.dump(new_consent)
 
 
-# class ConsentTargetResource(Resource):
-#     def get(self, targetId):
-#         post = Post.query.get_or_404(post_id)
-#         return post_schema.dump(post)
-#
-#     def patch(self, targetId):
-#         post = Post.query.get_or_404(post_id)
-#
-#         if 'title' in request.json:
-#             post.title = request.json['title']
-#         if 'content' in request.json:
-#             post.content = request.json['content']
-#
-#         db.session.commit()
-#         return post_schema.dump(post)
-#
+class ConsentTargetResource(Resource):
+    def get(self, targetId):
+        consents = Consent.query.filter_by(id=targetId).all()
+        return consents_schema.dump(consents)
+
+    def patch(self, targetId):
+        consent = Consent.query.filter_by(id=targetId).order_by(Consent.version.desc()).first()
+
+        new_consent = Consent(
+            id=consent.id,
+            name=consent.name,
+            consent_url=request.json['consent_url'],
+            version=consent.version+1
+        )
+        db.session.add(new_consent)
+        db.session.commit()
+        return consent_schema.dump(new_consent)
+
 api.add_resource(ConsentTargetsResource, '/consent/target')
-# api.add_resource(ConsentTargetResource, '/consent/target/<int:targetId>')
+api.add_resource(ConsentTargetResource, '/consent/target/<targetId>')
 
 # sanity check
 @app.route("/ping")
